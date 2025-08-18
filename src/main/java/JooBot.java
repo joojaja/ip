7a6282f16a04093ed79f060cb51d6a6aa48c4491
joojaja <194677730+joojaja@users.noreply.github.com>
2025-08-18 15:58:40 +0800

    rename chatbot, added greet and exit

---
+++ src/main/java/JooBot.java
@@ -0,0 +1,15 @@
+public class JooBot {
+    public static void main(String[] args) {
+        String text =
+                """
+                ____________________________________________________________
+                 Hello! I'm JooBot
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                
+                """;
+        System.out.println(text);
+    }
+}
