5a2df71e8bf3889ff18da304b59413ae232c2348
joojaja <194677730+joojaja@users.noreply.github.com>
2025-08-19 22:31:58 +0800

    Improve to Level-1

--- src/main/java/JooBot.java
+++ src/main/java/JooBot.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class JooBot {
     public static void main(String[] args) {
         String text =
                 """
-                ____________________________________________________________
-                 Hello! I'm JooBot
-                 What can I do for you?
-                ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________
+                    ____________________________________________________________
+                    Hello! I'm JooBot
+                    What can I do for you?
+                    ____________________________________________________________
                 
                 """;
         System.out.println(text);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println("    -------------------------------");
+            if (command.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!\n");
+                scanner.close();
+                return;
+            }
+            System.out.println("\t" + command);
+            System.out.println("    -------------------------------");
+        }
+
     }
 }
