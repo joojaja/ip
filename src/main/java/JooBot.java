74f90acae7c31bc59f328161cb301e6771d081d6
joojaja <194677730+joojaja@users.noreply.github.com>
2025-08-20 21:34:14 +0800

    Improve to Level-2

--- src/main/java/JooBot.java
+++ src/main/java/JooBot.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.util.Scanner; // for input
+import java.util.ArrayList; // for keeping track of list
 
 public class JooBot {
     public static void main(String[] args) {
@@ -8,23 +9,50 @@ public class JooBot {
                     Hello! I'm JooBot
                     What can I do for you?
                     ____________________________________________________________
-                
                 """;
-        System.out.println(text);
+        System.out.println(text); // starting greeting message
 
+        // create a dynamic list of strings
+        ArrayList<String> items = new ArrayList<>();
+
+        // used to get input
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String command = scanner.nextLine();
-            System.out.println("    -------------------------------");
-            if (command.equals("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!\n");
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(  "    ____________________________________________________________\n"
+                                   + "    Bye. Hope to see you again soon!\n"
+                                   + "    ____________________________________________________________");
                 scanner.close();
                 return;
+            } else if (input.equals("list")) {
+                listAllItem(items);
+            } else {
+                addItem(input, items);
             }
-            System.out.println("\t" + command);
-            System.out.println("    -------------------------------");
         }
+    }
 
+    public static void addItem(String item, ArrayList<String> list) {
+        list.add(item);
+        System.out.printf(
+                        """
+                            ____________________________________________________________
+                            added: %s
+                            ____________________________________________________________\n
+                        """, item
+                        );
+    }
+
+    public static void listAllItem(ArrayList<String> list) {
+        String items = "";
+        for (int i = 0; i < list.size(); i++) {
+            items += String.format("    %d. %s\n", i + 1, list.get(i));
+        }
+        items =   "    ____________________________________________________________\n"
+                + items
+                + "    ____________________________________________________________\n";
+        System.out.println(items);
     }
 }
