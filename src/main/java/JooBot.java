826099192df6a45c0796effc82034382098dd67a
joojaja <194677730+joojaja@users.noreply.github.com>
2025-08-20 22:13:34 +0800

    Improve to Level-3

--- src/main/java/JooBot.java
+++ src/main/java/JooBot.java
@@ -1,56 +1,93 @@
+import java.util.Objects;
 import java.util.Scanner; // for input
 import java.util.ArrayList; // for keeping track of list
 
 public class JooBot {
     public static void main(String[] args) {
-        String text =
+        String start =
                 """
                     ____________________________________________________________
                     Hello! I'm JooBot
                     What can I do for you?
                     ____________________________________________________________
                 """;
-        System.out.println(text); // starting greeting message
-
-        // create a dynamic list of strings
-        ArrayList<String> items = new ArrayList<>();
-
-        // used to get input
-        Scanner scanner = new Scanner(System.in);
+        System.out.println(start); // starting greeting message
+        ArrayList<Task> tasks = new ArrayList<>(); // create a dynamic list of strings
+        Scanner scanner = new Scanner(System.in); // used to get input
 
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(  "    ____________________________________________________________\n"
-                                   + "    Bye. Hope to see you again soon!\n"
-                                   + "    ____________________________________________________________");
-                scanner.close();
-                return;
-            } else if (input.equals("list")) {
-                listAllItem(items);
-            } else {
-                addItem(input, items);
+            String all_input = scanner.nextLine();
+            String[] text = all_input.split(" ", 2);
+            String start_word = text[0];
+
+            String input = start_word.equals("mark") || start_word.equals("unmark")
+                           ? start_word
+                           : all_input;
+
+            int index = start_word.equals("mark") || start_word.equals("unmark")
+                        ? Integer.parseInt(text[1]) - 1
+                        : 0;
+
+            switch (input) {
+                // end program
+                case "bye":
+                    System.out.println(  "    ____________________________________________________________\n"
+                            + "    Bye. Hope to see you again soon!\n"
+                            + "    ____________________________________________________________");
+                    scanner.close();
+                    return;
+
+                // list all tasks
+                case "list":
+                    listAllItem(tasks);
+                    break;
+
+                // change status to mark
+                case "mark":
+                    tasks.get(index).markDone();
+                    System.out.println(
+                                "    ____________________________________________________________\n" +
+                                "    Nice! I've marked this task as done:\n" +
+                                "      " + tasks.get(index) + "\n" +
+                                "    ____________________________________________________________");
+                    break;
+
+                // change status to unmark
+                case "unmark":
+                    tasks.get(index).markNotDone();
+                    System.out.println(
+                                    "    ____________________________________________________________\n" +
+                                    "    OK, I've marked this task as not done yet:\n" +
+                                    "      " + tasks.get(index) + "\n" +
+                                    "    ____________________________________________________________");
+                    break;
+
+                // add new task
+                default:
+                    addTask(input, tasks);
             }
         }
     }
 
-    public static void addItem(String item, ArrayList<String> list) {
-        list.add(item);
+    public static void addTask(String description, ArrayList<Task> list) {
+        Task task = new Task(description);
+        list.add(task);
         System.out.printf(
                         """
                             ____________________________________________________________
                             added: %s
                             ____________________________________________________________\n
-                        """, item
+                        """, task
                         );
     }
 
-    public static void listAllItem(ArrayList<String> list) {
+    public static void listAllItem(ArrayList<Task> list) {
         String items = "";
         for (int i = 0; i < list.size(); i++) {
-            items += String.format("    %d. %s\n", i + 1, list.get(i));
+            items += String.format("     %d. %s\n", i + 1, list.get(i));
         }
         items =   "    ____________________________________________________________\n"
+                + "     Here are the tasks in your list:\n"
                 + items
                 + "    ____________________________________________________________\n";
         System.out.println(items);
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String desc) {
+        this.description = desc;
+        this.isDone = false; // default false
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
+}
